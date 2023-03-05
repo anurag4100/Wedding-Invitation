0086205d385ed3db3df15ae5a001dc7f251e8200
anurag4100 <anuragbharatpandey@gmail.com>
2023-03-05 13:43:21 +0530

    insta feed and cleanup

--- js/script.js
+++ js/script.js
@@ -1,25 +1,12 @@
 /**
- * @author Anurag <vinitshahdeo@gmail.com>
+ * @author Anurag <anuragbharatpandey@gmail.com>
  */
 (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
 })(jQuery);
 
-/**
- *
- * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
- *
- * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
- * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
- * Let's keep the sky-high status of this song untouched!
- *
- * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
- * Choose a heart-touching track! 🎵 ❤️
- *
- * Listen here: https://youtu.be/X0MDALpV29s
- *
- */
+
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
@@ -29,7 +16,6 @@ $(document).on('click', function(){
 var countDownDate = new Date("May 02, 2023 00:00:00").getTime();
 
 
-// being a bit cool :p  
 var styles = [
     'background: linear-gradient(#D33106, #571402)'
     , 'border: 4px solid #3E0E02'
